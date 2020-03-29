7c7c0a5e0d5a3cc8c6888272a4f25e9729a8800a
lss2545 <lss2545@163.com>
2020-03-29 20:16:56 +0800

    ad-geteway

---
+++ ad-geteway/src/main/java/com/imooc/ad/ZuulGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by linshisheng on 2020/03/29.
+ *
+ * springCloud 实现的网关
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+}
