676318749e2294688297f81b434b913d1e829460
lss1887 <(W9002575)>
2020-04-07 21:56:16 +0800

    entity

--- ad-service/ad-sponsor/src/main/java/com/imooc/ad/SponsorApplication.java
+++ ad-service/ad-sponsor/src/main/java/com/imooc/ad/SponsorApplication.java
@@ -1,10 +1,18 @@
 package com.imooc.ad;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients //调用其他微服务 或则监控
+@EnableCircuitBreaker //断路器 监控
+@EnableEurekaClient //能够从其他微服务中拉取信息
 @SpringBootApplication
 public class SponsorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class,args);
+    }
 }
