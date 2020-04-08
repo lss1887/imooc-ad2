9de7c76a624a4921024d66ee9ecc0711ad0f2eee
lss1887 <(W9002575)>
2020-04-08 21:51:19 +0800

    createUser

--- ad-service/ad-sponsor/src/main/java/com/imooc/ad/service/impl/UserServiceImpl.java
+++ ad-service/ad-sponsor/src/main/java/com/imooc/ad/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.imooc.ad.dao.AdUserRepository;
 import com.imooc.ad.entity.AdUser;
 import com.imooc.ad.exception.AdException;
 import com.imooc.ad.service.IUserService;
+import com.imooc.ad.utils.CommonUtils;
 import com.imooc.ad.vo.CreateUserRequest;
 import com.imooc.ad.vo.CreateUserResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +27,7 @@ public class UserServiceImpl implements IUserService {
         if(oldUsername != null){
             throw new AdException(Constants.ErrorMsg.SAME_NAME_ERROR);
         }
-        return null;
+        AdUser newUser = userRepository.save(new AdUser(request.getUsername(), CommonUtils.md5(request.getUsername())));
+        return new CreateUserResponse(newUser.getId(),newUser.getUsername(),newUser.getToken(),newUser.getCreateTime(),newUser.getUpdateTime());
     }
 }
