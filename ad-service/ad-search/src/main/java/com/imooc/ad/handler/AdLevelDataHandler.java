0cbbdd5f980a3059617528f2933829e81fdf8988
lss1887 <(W9002575)>
2020-07-30 15:53:56 +0800

    AdLevelDataHandler

--- ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
@@ -1,10 +1,7 @@
 package com.imooc.ad.handler;
 
 import com.alibaba.fastjson.JSON;
-import com.imooc.ad.dump.table.AdCreativeTable;
-import com.imooc.ad.dump.table.AdCreativeUnitTable;
-import com.imooc.ad.dump.table.AdPlanTable;
-import com.imooc.ad.dump.table.AdUnitTable;
+import com.imooc.ad.dump.table.*;
 import com.imooc.ad.index.DataTable;
 import com.imooc.ad.index.IndexAware;
 import com.imooc.ad.index.adplan.AdPlanIndex;
@@ -15,9 +12,16 @@ import com.imooc.ad.index.creative.CreativeIndex;
 import com.imooc.ad.index.creative.CreativeObject;
 import com.imooc.ad.index.creativeunit.CreativeUnitIndex;
 import com.imooc.ad.index.creativeunit.CreativeUnitObject;
+import com.imooc.ad.index.district.UnitDstrictIndex;
+import com.imooc.ad.index.interest.UnitItIndex;
+import com.imooc.ad.index.keyword.UnitKeywordIndex;
 import com.imooc.ad.mysql.constant.OpType;
 import com.imooc.ad.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 索引存在层级关系 依赖关系的划分 有了推广计划才有推广单元
@@ -27,93 +31,130 @@ import lombok.extern.slf4j.Slf4j;
 public class AdLevelDataHandler {
     /**
      * 第二层级索引实现
+     *
      * @param planTable
      * @param type
      */
-    public  static void handlerLeverlAdPlan(AdPlanTable planTable,OpType type){
-            AdPlanObject planObject = new AdPlanObject(
-                    planTable.getId(),
-                    planTable.getUserId(),
-                    planTable.getPlanStatus(),
-                    planTable.getStartDate(),
-                    planTable.getEndDate()
-            );
-                handleBinLogEvent(DataTable.of(AdPlanIndex.class),planObject.getPlanId(),planObject,type);
+    public static void handleLeverl2(AdPlanTable planTable, OpType type) {
+        AdPlanObject planObject = new AdPlanObject(
+                planTable.getId(),
+                planTable.getUserId(),
+                planTable.getPlanStatus(),
+                planTable.getStartDate(),
+                planTable.getEndDate()
+        );
+        handleBinLogEvent(DataTable.of(AdPlanIndex.class), planObject.getPlanId(), planObject, type);
+    }
+
+    public static void handleLeverl2(AdCreativeTable creativeTable, OpType type) {
+        CreativeObject creativeObject = new CreativeObject(
+                creativeTable.getAdId(),
+                creativeTable.getName(),
+                creativeTable.getType(),
+                creativeTable.getMeterialType(),
+                creativeTable.getHeight(),
+                creativeTable.getWidth(),
+                creativeTable.getAuditStatus(),
+                creativeTable.getAdUrl()
+        );
+        handleBinLogEvent(DataTable.of(CreativeIndex.class), creativeObject.getAdId(), creativeObject, type);
+    }
+
+
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
+        AdPlanObject planObject = DataTable.of(AdPlanIndex.class).get(unitTable.getPlanId());
+        if (planObject == null) {
+            log.error("handleLevel3 found AdPlanObject error :{}", unitTable.getPlanId());
+            return;
         }
-            public  static void handlerLeverlAdCreative(AdCreativeTable creativeTable, OpType type){
-                CreativeObject creativeObject = new CreativeObject(
-                       creativeTable.getAdId(),
-                        creativeTable.getName(),
-                        creativeTable.getType(),
-                        creativeTable.getMeterialType(),
-                        creativeTable.getHeight(),
-                        creativeTable.getWidth(),
-                        creativeTable.getAuditStatus(),
-                        creativeTable.getAdUrl()
-                );
-                handleBinLogEvent(DataTable.of(CreativeIndex.class),creativeObject.getAdId(),creativeObject,type);
-            }
+        AdUnitObject unitObject = new AdUnitObject(
+                unitTable.getUnitId(),
+                unitTable.getUnitStatus(),
+                unitTable.getPositionType(),
+                unitTable.getPlanId(),
+                planObject
+        );
+        handleBinLogEvent(DataTable.of(AdUnitIndex.class), unitTable.getUnitId(), unitObject, type);
+    }
 
-
-        public static void handleLevel3(AdUnitTable unitTable,OpType type){
-            AdPlanObject planObject = DataTable.of(AdPlanIndex.class).get(unitTable.getPlanId());
-            if(planObject == null){
-                log.error("handleLevel3 found AdPlanObject error :{}",unitTable.getPlanId());
-                return;
-            }
-            AdUnitObject unitObject = new AdUnitObject(
-                    unitTable.getUnitId(),
-                    unitTable.getUnitStatus(),
-                    unitTable.getPositionType(),
-                    unitTable.getPlanId(),
-
-                    planObject
-            );
-            handleBinLogEvent(DataTable.of(AdUnitIndex.class),unitTable.getUnitId(),unitObject,type);
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable, OpType type) {
+        if (type == OpType.UPDATE) {
+            log.error("CreativeUnitIndex not support update");
+            return;
         }
-
-    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable, OpType type){
-            if(type == OpType.UPDATE){
-                log.error("CreativeUnitIndex not support update");
-                return;
-            }
         AdUnitObject unitObject = DataTable.of(AdUnitIndex.class).get(creativeUnitTable.getUnitId());
         CreativeObject creativeObject = DataTable.of(CreativeIndex.class).get(creativeUnitTable.getAdId());
 
-        if(unitObject == null || creativeObject == null){
+        if (unitObject == null || creativeObject == null) {
             log.error("AdcreativeUnitTable index error :{}", JSON.toJSONString(creativeUnitTable));
             return;
         }
 
         CreativeUnitObject creativeUnitObject = new CreativeUnitObject(creativeUnitTable.getAdId(), creativeUnitTable.getUnitId());
         handleBinLogEvent(DataTable.of(CreativeUnitIndex.class),
-                CommonUtils.stringConcat(creativeUnitObject.getAdId().toString(),creativeUnitObject.getUnitId().toString()),
+                CommonUtils.stringConcat(creativeUnitObject.getAdId().toString(), creativeUnitObject.getUnitId().toString()),
                 creativeUnitObject,
                 type);
 
 
     }
 
+    public static void handleLevel4(AdUnitDistrictTable unitDistrictTable,OpType type){
+        if(type == OpType.UPDATE){
+            log.error("AdUnitDistrictIndex not support update");
+            return;
+        }
+        AdUnitObject unitObject = DataTable.of(AdUnitIndex.class).get(unitDistrictTable.getUnitId());
+        if(unitObject == null){
+            log.error("AdUnitDistrictTable index error :{}",unitDistrictTable.getUnitId());
+            return;
+        }
+        String key = CommonUtils.stringConcat(unitDistrictTable.getProvince(),unitDistrictTable.getCity());
+        Set<Long> value =  new HashSet<>(Collections.singleton(unitDistrictTable.getUnitId()));
+
+        handleBinLogEvent(DataTable.of(UnitDstrictIndex.class),key,value,type);
+    }
+
+    public static void handleLevel4(AdUnitItTable unitItTable,OpType type){
+        if(type == OpType.UPDATE){
+            log.error("AdUnitItIndex not support update");
+            return;
+        }
+        AdUnitObject unitObject = DataTable.of(AdUnitIndex.class).get(unitItTable.getUnitId());
+        if(unitObject == null){
+            log.error("AdUnitItTable index error: {}",unitItTable.getUnitId());
+            return;
+        }
+
+        Set<Long> value =  new HashSet<>(Collections.singleton(unitItTable.getUnitId()));
+        handleBinLogEvent(DataTable.of(UnitItIndex.class),unitItTable.getItTag(),value,type);
+    }
+    public static void handleLevel4(AdUnitKeywordTable unitKeywordTable,OpType type){
+        if(type == OpType.UPDATE){
+            log.error("AdUnitKeyword not support update");
+            return;
+        }
+        AdUnitObject unitObject = DataTable.of(AdUnitIndex.class).get(unitKeywordTable.getUnitId());
+        if(unitObject == null){
+            log.error("AdUnitKeyword index error: {}",unitKeywordTable.getUnitId());
+            return;
+        }
+        Set<Long> value =  new HashSet<>(Collections.singleton(unitKeywordTable.getUnitId()));
+        handleBinLogEvent(DataTable.of(UnitKeywordIndex.class),unitKeywordTable.getKeyword(),value,type);
+    }
 
 
+    private static <K, V> void handleBinLogEvent(IndexAware<K, V> index, K key, V value, OpType type) {
 
-
-
-
-
-
-            
-    private static <K,V> void handleBinLogEvent(IndexAware<K,V> index, K key, V value, OpType type){
-
-        switch (type){
+        switch (type) {
             case ADD:
-                index.add(key,value);
+                index.add(key, value);
                 break;
             case UPDATE:
-                index.update(key,value);
+                index.update(key, value);
                 break;
             case DELETE:
-                index.delete(key,value);
+                index.delete(key, value);
                 break;
             default:
                 break;
